8e815183476bf99911bfae8317349c6ac4a27b49
Einar <einar.soderbergbeckman@elev.ga.ntig.se>
2025-05-09 14:41:09 +0200

    Kompletering

--- JS/login.js
+++ JS/login.js
@@ -18,9 +18,32 @@ document.getElementById("continueNew").addEventListener("click", function () {
 document.getElementById("newAccount").addEventListener("click", function () {
     document.getElementById("boxLogin").style.visibility = "collapse";
     document.getElementById("boxNew").style.visibility = "visible";
+    inputs = document.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = "";
+    }
 });
 
 document.getElementById("login").addEventListener("click", function () {
     document.getElementById("boxNew").style.visibility = "collapse";
     document.getElementById("boxLogin").style.visibility = "visible";
+    inputs = document.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = "";
+    }
+});
+
+document.getElementById("redirNew").addEventListener("click", function () {
+    document.getElementById("boxWelcome").style.visibility = "collapse";
+    document.getElementById("boxNew").style.visibility = "visible";
+});
+
+document.getElementById("redirLogIn").addEventListener("click", function () {
+    document.getElementById("boxWelcome").style.visibility = "collapse";
+    document.getElementById("boxLogin").style.visibility = "visible";
+});
+
+document.getElementById("redirMain").addEventListener("click", function () {
+    localStorage.setItem("activeUser", "Guest");
+    window.location.href = "../HTML/Main.html";
 });
